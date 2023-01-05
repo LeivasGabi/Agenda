50c7839475ae2f93b6c408a3afecda27937abe31
LeivasGabi <leivasgabi@gmail.com>
2023-01-04 21:14:35 -0300

    ajustes nos nomes das variaveis e edit e text

--- app/src/main/java/com/example/agendinha/model/Schedule.kt
+++ app/src/main/java/com/example/agendinha/model/Schedule.kt
@@ -3,6 +3,6 @@ package com.example.agendinha.model
 data class Schedule(
     var type: String,
     var hour: String,
-    var address: String,
+    var task: String,
     var date: String
 )
--- app/src/main/java/com/example/agendinha/view/TaskFragment.kt
+++ app/src/main/java/com/example/agendinha/view/TaskFragment.kt
@@ -5,14 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.agendinha.R
 import com.example.agendinha.model.Schedule
-import kotlinx.android.synthetic.main.fragment_service.*
 
 
-class ServiceFragment : Fragment(R.layout.fragment_service) {
+class TaskFragment : Fragment(R.layout.fragment_task) {
 
     private val args : ServiceFragmentArgs by navArgs()
 
@@ -22,18 +20,18 @@ class ServiceFragment : Fragment(R.layout.fragment_service) {
         //TODO salvar a o servico e mostrar na recyclerview
         //TODO dialog para salvo com sucesso
 
-//       view.findViewById<TextView>(R.id.type_service).text = args.
+       view.findViewById<TextView>(R.id.typeService).text = args.typeService.toString()
 
         val typeService = requireArguments()
-        val hour = view.findViewById<EditText>(R.id.serviceHour)
-        val date = view.findViewById<EditText>(R.id.serviceDate)
-        val address = view.findViewById<EditText>(R.id.serviceAddress)
+        val hour = view.findViewById<EditText>(R.id.editTextTaskHour)
+        val date = view.findViewById<EditText>(R.id.editTextTaskDate)
+        val task = view.findViewById<EditText>(R.id.editTextTask)
 
         val schudule = Schedule(
             type = typeService.toString(),
             hour = hour.text.toString(),
             date = date.text.toString(),
-            address = address.text.toString()
+            task = task.text.toString()
         )
     }
 }
